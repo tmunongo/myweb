75738430cf6ec63db4859567f5f557df7aaf89f1
Tawanda <ejmunongo@gmail.com>
2022-07-22 16:42:52 +0200

    add light theme

--- src/components/Switch.js
+++ src/components/Switch.js
@@ -1,19 +1,9 @@
 import React from 'react'
 
 const Switch = () => {
-  // const themes = getFromLS('all-themes')
-
-  const handleToggle = ({ themes }) => {
-    // setMode(themes.data.light)
-  }
   return (
     <>
-      <input
-        onClick={handleToggle}
-        className="switch"
-        id={`new-switch`}
-        type="checkbox"
-      />
+      <input className="switch" id={`new-switch`} type="checkbox" />
       <label className="switch-label" htmlFor={`new-switch`}>
         <span className={`switch-button`} />
       </label>
