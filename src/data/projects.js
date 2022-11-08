e0e76a7f18a82a046d9bae4a24190c5a8af97dd5
Tawanda <ejmunongo@gmail.com>
2022-11-08 17:14:32 +0200

    update projects page

--- src/data/projects.js
+++ src/data/projects.js
@@ -7,7 +7,7 @@ export const projects = [
       "Let's Feast is a web app designed with cooking enthusiasts and professionals in mind. It allows users to catalogue and organise their recipes. The web app leverages Google OAuth 2.0 for quick and easy authentication flows through Passport JS. Users can take advantage of Markdown to format their recipes.",
     preview:
       "https://user-images.githubusercontent.com/17217574/200599450-385f6d60-d119-4b05-99e9-fb1fb78c120c.png",
-    github: "https://github.com/edtha3rd/recipe-book-web",
+    github: "https://github.com/tmunongo/recipe-book-web",
     link: "https://lf.tawandamunongo.tech",
   },
   {
@@ -16,8 +16,8 @@ export const projects = [
     topics: ["NextJS", "TailWind CSS"],
     details:
       "I built the UI for the Twitch live-streaming site using NextJS and Tailwind CSS. Working authentication is implemented using NextAuth allowing users to log in with a Google or Github account.",
-    preview: "https://github.com/edtha3rd/twitch-next/raw/main/home_page.png",
-    github: "https://github.com/edtha3rd/twitch-next",
+    preview: "https://github.com/tmunongo/twitch-next/raw/main/home_page.png",
+    github: "https://github.com/tmunongo/twitch-next",
     link: "https://twitch-next-nu.vercel.app/",
   },
   {
@@ -27,8 +27,8 @@ export const projects = [
     details:
       "This is a simple website for finding out information about different countries around the world. It uses the REST Countries API to retrieve details like country name, capital city, region, population, currency, flag, and more.",
     preview:
-      "https://github.com/edtha3rd/countries-web/raw/master/screenshots/countries.png",
-    github: "https://github.com/edtha3rd/countries-web",
+      "https://github.com/tmunongo/countries-web/raw/master/screenshots/countries.png",
+    github: "https://github.com/tmunongo/countries-web",
     link: "https://country-query.surge.sh/",
   },
   {
@@ -38,8 +38,8 @@ export const projects = [
     details:
       "The app allows a user to register an account and view all the available movies in the database. A user can customize their reservation by picking a location, watch time, day, and a seat.",
     preview:
-      "https://github.com/edtha3rd/tickets-mob/raw/master/live/home_page.gif",
-    github: "https://github.com/edtha3rd/tickets-mob",
+      "https://github.com/tmunongo/tickets-mob/raw/master/live/home_page.gif",
+    github: "https://github.com/tmunongo/tickets-mob",
   },
   {
     id: 5,
@@ -47,8 +47,8 @@ export const projects = [
     topics: ["Java"],
     details:
       "A simple, lightweight text editor is a programmer's best friend. The basic functionality of the text editor must allow a user to open a new text file, save a new text file, update an existing file, or open a file from the device.",
-    preview: "https://github.com/edtha3rd/notepad/raw/master/notepad.jpg",
-    github: "https://github.com/edtha3rd/notepad",
+    preview: "https://github.com/tmunongo/notepad/raw/master/notepad.jpg",
+    github: "https://github.com/tmunongo/notepad",
   },
   {
     id: 6,
@@ -56,9 +56,9 @@ export const projects = [
     topics: ["JavaScript"],
     details:
       "This is a web-based snake game in JavaScript. I chose this project to improve my understanding of how vanilla JavaScript refreshes rendered content and how to manually control this to implement something like a game engine.",
-    preview: "https://github.com/edtha3rd/snake-game/raw/master/gameplay.png",
-    github: "https://github.com/edtha3rd",
-    link: "https://edtha3rd.github.io/snake-game/",
+    preview: "https://github.com/tmunongo/snake-game/raw/master/gameplay.png",
+    github: "https://github.com/tmunongo",
+    link: "https://tmunongo.github.io/snake-game/",
   },
   {
     id: 7,
@@ -66,8 +66,8 @@ export const projects = [
     topics: ["GatsbyJS", "Styled Components"],
     details:
       "A static site landing page designed and built for a fashion brand selling hair pieces.",
-    preview: "https://github.com/edtha3rd/snake-game/raw/master/gameplay.png",
-    github: "https://github.com/edtha3rd/hair-by-a",
+    preview: "https://github.com/tmunongo/snake-game/raw/master/gameplay.png",
+    github: "https://github.com/tmunongo/hair-by-a",
     link: "https://hairbya.gatsbyjs.io/",
   },
 ];
